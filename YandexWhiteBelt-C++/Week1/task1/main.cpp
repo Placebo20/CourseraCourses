6b0bfc728a6fb37e6fa74b7208721811a5292315
Placebo20 <dimag586@gmail.com>
2022-01-23 12:47:00 +0200

    Courses init

---
+++ YandexWhiteBelt-C++/Week1/task1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+	int in1 = 0;
+	int in2= 0;
+	std::cout << "Input Number[1]:";
+	std::cin >> in1;
+	std::cout << "Input Number[1]:";
+	std::cin >> in2;
+	std::cout << in1 + in2;
+	return 0;
+}
